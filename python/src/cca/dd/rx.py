f603970ed23351e8a49f07cf17046d2b13ee0396
Masatomo Hashimoto <m.hashimoto@stair.center>
2024-05-11 08:47:57 +0900

    snap

--- python/src/cca/dd/rx.py
+++ python/src/cca/dd/rx.py
@@ -22,6 +22,7 @@ __author__ = 'Masatomo Hashimoto <m.hashimoto@stair.center>'
 
 import os
 import json
+import csv
 import traceback
 import logging
 
@@ -34,61 +35,96 @@ from cca.ccautil.common import setup_logger, DEFAULT_LOGGING_LEVEL
 logger = logging.getLogger()
 
 RX_JAR_PATH = os.getenv('RX_JAR_PATH', None)
+PATHTBL_FILE_NAME = 'pathtbl.csv'
 
 
-def execute_ref(projs_path, proj_id, cid, ref, inv=False):
-
-    if inv:
-        suffix = '-after'
-        get_opts = ref.get_inv_options
-    else:
-        suffix = '-before'
-        get_opts = ref.get_options
-
-    ref_id = ref.get_id()
-    ws_path = os.path.join(WORK_DIR, proj_id, cid)
-    proj_path = os.path.join(projs_path, proj_id, f'{cid}{suffix}')
-    ensure_dir(ws_path)
-    opts = get_opts(ws_path, proj_path) + f' -Dproject.name={ref_id}{suffix}'
-    cmd = f'java {opts} -jar {RX_JAR_PATH}'
-    logger.debug(f'cmd={cmd}')
-    system(cmd)
+def load_pathtbl(path):
+    tbl = {}
+    with open(path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            tbl[row[0]] = row[1]
+    return tbl
 
 
-def execute_refs(projs_path, ref_dir=REFACT_DIR):
-    logger.info(f'projs_path={projs_path}')
-    logger.info(f'ref_dir={ref_dir}')
+class Executor(object):
+    def __init__(self, projs_path, ref_dir=REFACT_DIR):
+        logger.info(f'projs_path={projs_path}')
+        logger.info(f'ref_dir={ref_dir}')
+        if not os.path.exists(ref_dir):
+            logger.error(f'not found: "{ref_dir}"')
+            raise FileNotFoundError
+        self.projs_path = projs_path
+        self.ref_dir = ref_dir
+        self.merge_scenario_count = 0
+        self.merge_scenario_list = []
 
-    if not os.path.exists(ref_dir):
-        logger.error(f'not found: "{ref_dir}"')
-        return
+    def dump_merge_scenario_list(self, path):
+        if self.merge_scenario_list:
+            logger.info(f'dumping into {path}...')
+            with open(path, 'w') as f:
+                json.dump(self.merge_scenario_list, f)
+            logger.info(f'dumped {len(self.merge_scenario_list)} merge scenarios')
 
-    for uname in os.listdir(ref_dir):
-        for rname in os.listdir(os.path.join(ref_dir, uname)):
-            proj_id = f'{uname}/{rname}'
-            proj_dir = os.path.join(ref_dir, proj_id)
+    def execute_ref(self, proj_id, cid, ref, inv=False):
+        if inv:
+            suffix = '-after'
+            get_opts = ref.get_inv_options
+            src_loc = ref.desc_.loc
+        else:
+            suffix = '-before'
+            get_opts = ref.get_options
+            src_loc = ref.desc.loc
 
-            if not os.path.isdir(proj_dir):
-                continue
+        ref_id = ref.get_id()
+        ws_path = os.path.join(WORK_DIR, proj_id, cid)
+        proj_path = os.path.join(self.projs_path, proj_id, f'{cid}{suffix}')
+        ensure_dir(ws_path)
+        proj_name = f'{ref_id}{suffix}'
+        opts = get_opts(ws_path, proj_path) + f' -Dproject.name={proj_name}'
+        cmd = f'java {opts} -jar {RX_JAR_PATH}'
+        logger.debug(f'cmd={cmd}')
+        system(cmd)
+        pathtbl_path = os.path.join(ws_path, proj_name, PATHTBL_FILE_NAME)
+        pathtbl = load_pathtbl(pathtbl_path)
+        src_path = os.path.join(os.path.abspath(proj_path), src_loc)
+        dest_path = ws_path + pathtbl[src_path]
+        return (src_path, dest_path)
 
-            logger.info(f'proj_id={proj_id}')
+    def execute_refs(self):
+        for uname in os.listdir(self.ref_dir):
+            for rname in os.listdir(os.path.join(self.ref_dir, uname)):
+                proj_id = f'{uname}/{rname}'
+                proj_dir = os.path.join(self.ref_dir, proj_id)
 
-            ref_keys_path = os.path.join(proj_dir, 'ref_keys.json')
-            logger.info(f'loading "{ref_keys_path}"...')
-            try:
-                with open(ref_keys_path) as f:
-                    for cid, rtbl in json.load(f).items():
-                        logger.info(f'cid={cid}')
-                        for ra, rl in rtbl.items():
-                            for r in rl:
-                                ref = Ref.from_dict(r)
-                                if ref.has_descs():
-                                    logger.info(f'{ref}')
-                                    execute_ref(projs_path, proj_id, cid, ref)
-                                    execute_ref(projs_path, proj_id, cid, ref, inv=True)
-            except Exception:
-                traceback.print_exc()
-                continue
+                if not os.path.isdir(proj_dir):
+                    continue
+
+                logger.info(f'proj_id={proj_id}')
+
+                ref_keys_path = os.path.join(proj_dir, 'ref_keys.json')
+                logger.info(f'loading "{ref_keys_path}"...')
+                try:
+                    with open(ref_keys_path) as f:
+                        for cid, rtbl in json.load(f).items():
+                            logger.info(f'cid={cid}')
+                            for ra, rl in rtbl.items():
+                                for r in rl:
+                                    ref = Ref.from_dict(r)
+                                    if ref.has_descs():
+                                        logger.info(f'{ref}')
+                                        pathb, path0 = self.execute_ref(proj_id, cid, ref)
+                                        pathm, path1 = self.execute_ref(proj_id, cid, ref, inv=True)
+                                        self.merge_scenario_count += 1
+                                        d = {
+                                            'id': self.merge_scenario_count,
+                                            'proj_id': proj_id,
+                                            'instance': [pathb, path0, path1, pathm],
+                                        }
+                                        self.merge_scenario_list.append(d)
+                except Exception:
+                    traceback.print_exc()
+                    continue
 
 
 def main():
@@ -98,6 +134,10 @@ def main():
                             formatter_class=ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('projs_path', type=str, help='projects directory')
+
+    parser.add_argument('-o', '--merge-scenario-list-file', type=str,
+                        metavar='JSON_FILE', default='merges-rx.json',
+                        help='dump merge scenarios into JSON_FILE')
 
     parser.add_argument('-d', '--debug', dest='debug', action='store_true',
                         help='enable debug printing')
@@ -119,7 +159,9 @@ def main():
                  log_level,
                  log_file=log_file)
 
-    execute_refs(args.projs_path)
+    executor = Executor(args.projs_path)
+    executor.execute_refs()
+    executor.dump_merge_scenario_list(args.merge_scenario_list_file)
 
 
 if __name__ == '__main__':
