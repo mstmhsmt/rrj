cd9b17d8ec0381f4e329ea6c1f3deb0d413f30b0
Masatomo Hashimoto <m.hashimoto@stair.center>
2024-07-12 21:43:11 +0900

    rx parallelized

--- python/src/cca/dd/rx.py
+++ python/src/cca/dd/rx.py
@@ -26,22 +26,33 @@ import csv
 import traceback
 import subprocess
 import logging
+import sys
+import multiprocessing as mp
+import time
 
 from .ref import Ref
 from .common import LOG_DIR, REFACT_DIR, WORK_DIR
 from .misc import ensure_dir
 from .scan_oracle import scan_oracle
+from . import misc
 from cca.ccautil.proc import system
 from cca.ccautil.sloccount import sloccount_for_lang
 from cca.ccautil.common import setup_logger, DEFAULT_LOGGING_LEVEL
 from cca.ccautil.java_token_diff import all_different
+from cca.ccautil import sloccount
 
-logger = logging.getLogger()
+# logger = logging.getLogger()
+logger = mp.get_logger()
 
 RX_JAR_PATH = os.getenv('RX_JAR_PATH', None)
 PATHTBL_FILE_NAME = 'pathtbl.csv'
 DEFAULT_TARGET_REF = 'RV'
 TARGET_REF_LIST = DEFAULT_TARGET_REF.split(':')
+
+
+LOGGING_FORMAT = ('[%(asctime)s][%(processName)s]'
+                  '[%(levelname)s][%(module)s][%(funcName)s] %(message)s')
+NPROCS = 2
 
 
 def set_kkkklv(tbl, k1, k2, k3, k4, v):
@@ -87,6 +98,21 @@ def mangleM(i):
     return i_
 
 
+def init_proc(log_level, suffix):
+    pid = mp.current_process().name
+    log_dir = os.path.join(LOG_DIR, 'rx')
+    log_file = os.path.join(log_dir, f'rx.{pid}{suffix}.log')
+    fh = logging.FileHandler(log_file, mode='w', encoding='utf-8')
+    fh.setLevel(log_level)
+    fmt = logging.Formatter(LOGGING_FORMAT)
+    fh.setFormatter(fmt)
+    logger = mp.get_logger()
+    logger.addHandler(fh)
+    logger.propagate = False
+    sloccount.logger = logger
+    misc.logger = logger
+
+
 class Executor(object):
     def __init__(self, samples_path, ref_dir=REFACT_DIR):
         logger.info(f'samples_path={samples_path}')
@@ -97,6 +123,29 @@ class Executor(object):
         self.samples_path = samples_path
         self.ref_dir = ref_dir
         self.merge_scenario_count = 0
+        self.cmd_cache = {}   # cmd -> path_tbl
+        self.sloc_cache = {}  # path -> sloc
+
+    def count_sloc(self, path, nretries=2):
+        try:
+            return self.sloc_cache[path]
+        except KeyError:
+            count = 0
+            while count <= nretries:
+                if os.path.exists(path):
+                    break
+                else:
+                    logger.warning(f'not found: {path}')
+                    time.sleep(2**count)
+                    count += 1
+                    logger.warning(f'retrying ({count})...')
+
+            datadir = os.path.join(WORK_DIR, f'slocdata-{mp.current_process().name}')
+            ensure_dir(datadir)
+
+            sloc = sloccount_for_lang('java', path, datadir=datadir)
+            self.sloc_cache[path] = sloc
+            return sloc
 
     def execute_ref(self, proj_id, cid, ref, inv=False, swap=False, mangler=None):
         mangling = ''
@@ -119,43 +168,73 @@ class Executor(object):
         opts = get_opts(ws_path, proj_path, mangler, swap=swap) + f' -Dproject.name={proj_name}'
         cmd = f'java {opts} -jar {RX_JAR_PATH}'
         logger.debug(f'cmd={cmd}')
-        debug_flag = logger.getEffectiveLevel() == logging.DEBUG
-        if debug_flag:
-            try:
-                p = subprocess.run(cmd, shell=True, cwd=None, capture_output=True,
-                                   encoding=None, errors=None,
-                                   text=None, universal_newlines=None)
-                out = p.stdout
-                rc = p.returncode
-            except subprocess.CalledProcessError as e:
-                logger.warning(f'"{cmd}":'
-                               f' terminated abnormally (exitcode={e.returncode})')
-                out = e.output
-                rc = 1
-            logger.debug(out.decode('utf-8'))
-        else:
-            rc = system(cmd)
-        if rc == 0:
-            pathtbl_path = os.path.join(ws_path, proj_name, PATHTBL_FILE_NAME)
-            pathtbl = load_pathtbl(pathtbl_path)
+
+        if cmd in self.cmd_cache:
+            pathtbl = self.cmd_cache[cmd]
             src_path = os.path.join(os.path.abspath(proj_path), src_loc)
             dest_path = ws_path + pathtbl[src_path]
             return (src_path, dest_path)
-        else:
-            logger.warning('failed to execute rx')
-            return (None, None)
 
-    def make_merge_scenario(self, proj_id, cid, pathb, path1, path2, pathm):
+        if cmd not in self.cmd_cache:
+            debug_flag = logger.getEffectiveLevel() == logging.DEBUG
+            if debug_flag:
+                try:
+                    p = subprocess.run(cmd, shell=True, cwd=None, capture_output=True,
+                                       encoding=None, errors=None,
+                                       text=None, universal_newlines=None)
+                    out = p.stdout
+                    rc = p.returncode
+                except subprocess.CalledProcessError as e:
+                    logger.warning(f'"{cmd}":'
+                                   f' terminated abnormally (exitcode={e.returncode})')
+                    out = e.output
+                    rc = 1
+                logger.debug(out.decode('utf-8'))
+            else:
+                rc = system(cmd)
+            if rc == 0:
+                pathtbl_path = os.path.join(ws_path, proj_name, PATHTBL_FILE_NAME)
+                pathtbl = load_pathtbl(pathtbl_path)
+                self.cmd_cache[cmd] = pathtbl
+                src_path = os.path.join(os.path.abspath(proj_path), src_loc)
+                dest_path = ws_path + pathtbl[src_path]
+                return (src_path, dest_path)
+            else:
+                logger.warning('failed to execute rx')
+                return (None, None)
+
+    def check_ref(self, proj_id, cid, ref, inv=False, swap=False, mangler=None):
+        mangling = ''
+        if mangler is not None:
+            mangling = f'{mangler('')}'
+        if inv:
+            suffix = '-after'
+            get_opts = ref.get_options_
+        else:
+            suffix = '-before'
+            get_opts = ref.get_options
+
+        ref_id = ref.get_id()
+        ws_path = os.path.join(WORK_DIR, proj_id, cid)
+        proj_path = os.path.join(self.samples_path, proj_id, f'{cid}{suffix}')
+        ensure_dir(ws_path)
+        proj_name = f'{ref_id}{suffix}{mangling}'
+        opts = get_opts(ws_path, proj_path, mangler, swap=swap) + f' -Dproject.name={proj_name}'
+        cmd = f'java {opts} -jar {RX_JAR_PATH}'
+        return cmd
+
+    def make_merge_scenario(self, proj_id, cid, pathb, path1, path2, pathm, add_sloc=True):
         self.merge_scenario_count += 1
         instance = [pathb, path1, path2, pathm]
-        slocs = [sloccount_for_lang('java', p) for p in instance]
         d = {
             'id': self.merge_scenario_count,
             'proj_id': proj_id,
             'cid': cid,
             'instance': instance,
-            'slocs': slocs,
         }
+        if add_sloc:
+            slocs = [self.count_sloc(p) for p in instance]
+            d['slocs'] = slocs
         return d
 
     def extract_merge_scenario(self, proj_id, cid, ref, mangle=True):
@@ -190,6 +269,30 @@ class Executor(object):
             d['key'] = ref.key
         return d
 
+    def check_merge(self, proj_id, cid, ref, mangle=True):
+        d = None
+        if mangle:
+            cmd0 = self.check_ref(proj_id, cid, ref, swap=True, mangler=mangleB)
+            cmd1 = self.check_ref(proj_id, cid, ref, mangler=mangleM)
+            cmd2 = self.check_ref(proj_id, cid, ref, inv=True, swap=True, mangler=mangleM)
+            cmd3 = self.check_ref(proj_id, cid, ref, inv=True, mangler=mangleB)
+            d = [cmd0, cmd1, cmd2, cmd3]
+        else:
+            cmd0 = self.check_ref(proj_id, cid, ref)
+            cmd1 = self.execute_ref(proj_id, cid, ref, inv=True)
+            d = [cmd0, cmd1]
+        return d
+
+    def extract_merge_scenario_mp(self, tid_args):
+        tid, args = tid_args
+        proj_id, cid, refl, mangle, verdict, k = args
+        dl = []
+        for ref in refl:
+            d = self.extract_merge_scenario(proj_id, cid, ref, mangle=mangle)
+            dl.append(d)
+        pid = mp.current_process().name
+        return (pid, tid, (proj_id, cid, verdict, k, dl))
+
     def execute_refs(self, scenarios_path):
         merge_scenario_list = []
 
@@ -220,6 +323,73 @@ class Executor(object):
                 except Exception:
                     traceback.print_exc()
                     continue
+
+        if merge_scenario_list:
+            logger.info(f'dumping into {scenarios_path}...')
+            with open(scenarios_path, 'w') as f:
+                json.dump(merge_scenario_list, f)
+            logger.info(f'dumped {self.merge_scenario_count} merge scenarios')
+
+    def execute_refs_mp(self, scenarios_path, nprocs=NPROCS, log_level=None):
+
+        tasks = []
+        total_nrefs = 0
+
+        for uname in os.listdir(self.ref_dir):
+            for rname in os.listdir(os.path.join(self.ref_dir, uname)):
+                proj_id = f'{uname}/{rname}'
+                proj_dir = os.path.join(self.ref_dir, proj_id)
+
+                if not os.path.isdir(proj_dir):
+                    continue
+
+                logger.info(f'proj_id={proj_id}')
+
+                ref_keys_path = os.path.join(proj_dir, 'ref_keys.json')
+                logger.info(f'loading "{ref_keys_path}"...')
+                try:
+                    with open(ref_keys_path) as f:
+                        for cid, rtbl in json.load(f).items():
+                            logger.info(f'cid={cid}')
+                            for ra, rl in rtbl.items():
+                                for r in rl:
+                                    ref = Ref.from_dict(r)
+                                    if ref.has_descs():
+                                        logger.info(f'{ref}')
+                                        tasks.append((proj_id, cid, [ref], True, None, None))
+                                        total_nrefs += 1
+                except Exception:
+                    traceback.print_exc()
+                    continue
+
+        xl = []
+        for tid, task in enumerate(tasks):
+            xl.append((tid, task))
+
+        init = None
+        initargs = None
+        if log_level is not None:
+            init = init_proc
+            initargs = (log_level, '.rrj')
+
+        done = 0
+
+        merge_scenario_list = []
+
+        with mp.Pool(processes=nprocs, initializer=init, initargs=initargs) as pool:
+            for pid, tid, result in pool.imap_unordered(self.extract_merge_scenario_mp, xl,
+                                                        chunksize=1):
+                _, _, _, _, dl = result
+                n = 0
+                for d in dl:
+                    n += 1
+                    if d is not None:
+                        merge_scenario_list.append(d)
+                done += n
+                r = float(done) * 100.0 / total_nrefs
+                sys.stdout.write(f' processed {done:4}/{total_nrefs:4} ({r:2.2f}%)\r')
+                sys.stdout.flush()
+                logger.info(f'[{pid}] processed {n} in task-{tid} ({done}/{total_nrefs}={r:.2f}%)')
 
         if merge_scenario_list:
             logger.info(f'dumping into {scenarios_path}...')
@@ -337,6 +507,92 @@ class Executor(object):
                 json.dump(merge_scenario_tbl, f)
             logger.info(f'dumped {self.merge_scenario_count} merge scenarios')
 
+    def execute_oracle_refs_mp(self, oracle_path, scenarios_path, target_refs=TARGET_REF_LIST,
+                               nprocs=NPROCS, log_level=None):
+        if not os.path.exists(oracle_path):
+            logger.error(f'not found: "{oracle_path}"')
+            return
+
+        oracle = scan_oracle(oracle_path, out_path='oracle.json')
+
+        tasks = []
+        total_nrefs = 0
+
+        for proj_id, ctbl in oracle.items():
+            logger.info(f'proj_id={proj_id}')
+
+            for cid, oracle_rtbl in ctbl.items():
+                logger.info(f'cid={cid}')
+
+                for ra, oracle_vtbl in oracle_rtbl.items():
+                    if ra not in target_refs:
+                        continue
+
+                    logger.info(f'ra={ra}')
+
+                    _tp_keyl = [tuple(x) for x in oracle_vtbl.get('TP', [])]
+                    _ctp_keyl = [tuple(x) for x in oracle_vtbl.get('CTP', [])]
+                    _fp_keyl = [tuple(x) for x in oracle_vtbl.get('FP', [])]
+
+                    tp_keys = set([x[0] for x in _tp_keyl])
+                    ctp_keys = set([x[0] for x in _ctp_keyl])
+                    fp_keys = set([x[0] for x in _fp_keyl])
+
+                    # atp_keys = tp_keys | ctp_keys
+
+                    logger.info(f'ntp={len(tp_keys)} nctp={len(ctp_keys)} nfp={len(fp_keys)}')
+
+                    for verdict, keys in [('TP', tp_keys), ('CTP', ctp_keys), ('FP', fp_keys)]:
+                        logger.info(f'verdict={verdict}')
+
+                        for k in keys:
+                            candl = self.get_ref_candidates(proj_id, cid, k)
+                            logger.info(f'{k}: {len(candl)} candidates found')
+                            if candl:
+                                refl = []
+                                for cand in candl:
+                                    ref = Ref.from_dict(cand)
+                                    logger.info(f'ref={ref}')
+                                    refl.append(ref)
+                                    total_nrefs += 1
+
+                                tasks.append((proj_id, cid, refl, True, verdict, k))
+
+        xl = []
+        for tid, task in enumerate(tasks):
+            xl.append((tid, task))
+
+        init = None
+        initargs = None
+        if log_level is not None:
+            init = init_proc
+            initargs = (log_level, '.oracle')
+
+        done = 0
+
+        merge_scenario_tbl = {}
+
+        with mp.Pool(processes=nprocs, initializer=init, initargs=initargs) as pool:
+            for pid, tid, result in pool.imap_unordered(self.extract_merge_scenario_mp, xl,
+                                                        chunksize=1):
+                proj_id, cid, verdict, k, dl = result
+                n = 0
+                for d in dl:
+                    n += 1
+                    if d is not None:
+                        set_kkkklv(merge_scenario_tbl, proj_id, cid, verdict, k, d)
+                done += n
+                r = float(done) * 100.0 / total_nrefs
+                sys.stdout.write(f' processed {done:4}/{total_nrefs:4} ({r:2.2f}%)\r')
+                sys.stdout.flush()
+                logger.info(f'[{pid}] processed {n} in task-{tid} ({done}/{total_nrefs}={r:.2f}%)')
+
+        if merge_scenario_tbl:
+            logger.info(f'dumping into {scenarios_path}...')
+            with open(scenarios_path, 'w') as f:
+                json.dump(merge_scenario_tbl, f)
+            logger.info(f'dumped {self.merge_scenario_count} merge scenarios')
+
 
 def main():
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
@@ -357,6 +613,10 @@ def main():
                         metavar='JSON_FILE', default='merges-oracle-rx.json',
                         help='dump oracle merge scenarios into JSON_FILE')
 
+    parser.add_argument('-p', '--nprocs', dest='nprocs', type=int,
+                        default=NPROCS,
+                        help='specify number of processes')
+
     parser.add_argument('-d', '--debug', dest='debug', action='store_true',
                         help='enable debug printing')
 
@@ -371,15 +631,32 @@ def main():
     if args.debug:
         log_level = logging.DEBUG
 
-    log_file = os.path.join(LOG_DIR, 'rx.log')
+    log_dir = os.path.join(LOG_DIR, 'rx')
+    log_file = os.path.join(log_dir, 'rx.log')
+    ensure_dir(log_dir)
 
     setup_logger(logger,
                  log_level,
                  log_file=log_file)
 
+    nprocs = args.nprocs
+
     executor = Executor(args.samples_path)
-    executor.execute_refs(args.merge_scenario_list_file)
-    executor.execute_oracle_refs(args.oracle, args.merge_scenario_tbl_file)
+    # executor.execute_refs(args.merge_scenario_list_file)
+    # executor.execute_oracle_refs(args.oracle, args.merge_scenario_tbl_file)
+
+    if nprocs > 1:
+        print('rx for rrj in progress...')
+        executor.execute_refs_mp(args.merge_scenario_list_file,
+                                 nprocs=nprocs, log_level=log_level)
+        print('\nrx for oracle in progress...')
+        executor.execute_oracle_refs_mp(args.oracle, args.merge_scenario_tbl_file,
+                                        nprocs=nprocs, log_level=log_level)
+    else:
+        print('rx for rrj in progress...')
+        executor.execute_refs(args.merge_scenario_list_file)
+        print('rx for oracle in progress...')
+        executor.execute_oracle_refs(args.oracle, args.merge_scenario_tbl_file)
 
 
 if __name__ == '__main__':
